386d945ea8b6ad99f5967649e866058bc2a65469
icekidtech <princekelvin7568@gmail.com>
2025-06-19 03:05:27 +0100

    refactor: restructure App component to use Routes and define application routes

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,12 +1,28 @@
-import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./AppRoutes";
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import PaymentDemo from "../pages/PaymentDemo";
+import Dashboard from "../pages/Dashboard";
+import MerchantDashboard from "../pages/MerchantDashboard";
+import ShopperDashboard from "../pages/ShopperDashboard";
+import AdminDashboard from "../pages/AdminDashboard";
+import Transactions from "../pages/Transactions";
 
-function App() {
+function AppRoutes() {
   return (
-    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-      <AppRoutes />
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/payment-demo" element={<PaymentDemo />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/merchant" element={<MerchantDashboard />} />
+      <Route path="/shopper" element={<ShopperDashboard />} />
+      <Route path="/admin" element={<AdminDashboard />} />
+      <Route path="/transactions" element={<Transactions />} />
+    </Routes>
   );
 }
 
-export default App;
+export default AppRoutes;
